19e2e1cd211da86da4566c1c294e1484691f4055
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-03-10 17:10:03 +0100

    marble-shell-filled: improve warning for marble-shell attribute

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -12,9 +12,12 @@
    marble-shell-filled = callPackage ./marble-shell-filled {};
    
    ### marble-shell to marble-shell-filled with warning when this attribute is called
-   marble-shell = lib.warn 
-      "marble-shell has been renamed to marble-shell-filled, consider migrating to this new name before deleting this attribute in 1 month. (nur.repos.minegameYTB.theme.marble-shell -> minegameYTB.theme.marble-shell-filled)" 
-      (callPackage ./marble-shell-filled {});
+   marble-shell = let
+      buildPackage = callPackage ./marble-shell-filled {};
+    in 
+      builtins.trace
+        "marble-shell has been renamed to marble-shell-filled, consider migrating to this new name before deleting this attribute in 1 month. (nur.repos.minegameYTB.theme.marble-shell -> minegameYTB.theme.marble-shell-filled)"
+      buildPackage;
  };
 
  # some-qt5-package = libsForQt5.callPackage ./some-qt5-package { };
