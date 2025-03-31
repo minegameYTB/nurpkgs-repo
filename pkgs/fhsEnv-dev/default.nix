81b3417700668ee17e95f5dd4b937a6a032c2359
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-03-31 22:59:12 +0200

    fhsEnv-dev: change option and add test

--- pkgs/fhsEnv-dev/default.nix
+++ pkgs/fhsEnv-dev/default.nix
@@ -1,6 +1,5 @@
 {
  stdenvNoCC,
- stdenv,
  buildFHSEnv,
  lib,
  libsForQt5,
@@ -11,7 +10,8 @@
  writeScript,
  kernel-tools ? false,  ### Option to enable kernel development tools
  buildroot-tools ? false,  ### Option to enable Buildroot-specific tools
- useClang ? false
+ useClang ? false, ### Enable this to use clang instead of gcc
+ test ? false
 }:
 
 let
@@ -29,7 +29,7 @@ let
       ncurses5
       (if useClang then clang else gcc)
       gnumake
-      patch
+      gnupatch
       git
       gnutar
       gzip
@@ -57,11 +57,18 @@ let
       texinfo
     ] ++ lib.optionals kernel-tools ([
       ### Kernel-specific tools (enabled with kernel-tools = true)
-      ncurses5.dev
-      libsForQt5.qt5.qtbase.dev
       libsForQt5.qt5.qtbase
       libcap_ng
       pciutils
+
+      ### Rust tools
+      rustc
+      rust-bindgen
+      
+      ### Headers
+      openssl.dev
+      ncurses5.dev
+      libsForQt5.qt5.qtbase.dev
     ] ++ pkgs.linux.nativeBuildInputs) ++ lib.optionals buildroot-tools ([
       ### Buildroot-specific tools (enabled with buildroot-tools = true) 
       ### When kernel-tools is not used, include these tools in case
@@ -72,11 +79,17 @@ let
       texinfo
 
       ### Buildroot dependancies in additions to other tools
+      patchutils
       swig
       gperf
       libtool
       libmpc
+      libelf
       mpfr
+      gmp
+    ]) ++ lib.optionals test ([
+      cowsay
+      hello
     ]);
 
     ### Shell script that run automacally when enterred in this environment
@@ -95,7 +108,7 @@ let
         export QT_QPA_PLATFORM_PLUGIN_PATH="${libsForQt5.qt5.qtbase.bin}/lib/qt-${libsForQt5.qt5.qtbase.version}/plugins"
       ''}
       ### Custom PS1 for the shell environment (NixOS style)
-      export PROMPT_COMMAND='PS1="\[\e[1;32m\][fhsEnv-shell:\w]:\$\[\e[0m\] "'
+      export PROMPT_COMMAND='PS1="\[\e[1;32m\][fhsEnv-shell:\w]\$\[\e[0m\] "'
 
       exec ${bashInteractive}/bin/bash
     '';
@@ -104,7 +117,7 @@ in stdenv.mkDerivation {
   pname = "fhsEnv-shell";
   version = gcc.version;
 
-  ### stdenv options to disable unnecessary phases
+  ### stdenv options
   dontUnpack = true;
   dontBuild = true;
   dontConfigure = true;
