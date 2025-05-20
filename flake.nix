16ebbb11669d35f5cf8f2a3fa436e90983c9c9ae
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-05-20 23:36:49 +0200

    flake.nix: pass overlay to all outputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "My personal NUR repository";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay = {
@@ -7,28 +8,30 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   outputs = { self, nixpkgs, rust-overlay }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-      pkgs = import nixpkgs {
-         ### Import rust-overlay to pkgs attribute
-        inherit system;
-        overlays = [ rust-overlay.overlays.default ];
-      };
     in
     {
-      legacyPackages = forAllSystems (system:
-        let
-          ### Add rust-overlay
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ rust-overlay.overlays.default ];
-          };
-        in
-        import ./default.nix {
-          inherit pkgs;
+      ### Add overlay on any nix expression
+      pkgsForAll = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
         }
       );
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+
+      legacyPackages = forAllSystems (system:
+        let 
+          pkgs = self.pkgsForAll.${system};
+        in
+        import ./default.nix { inherit pkgs; }
+      );
+
+      packages = forAllSystems (system:
+        nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
+      );
     };
 }
+
