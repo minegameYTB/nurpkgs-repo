2c2d31edafb3d3c4d01b3fc81ffe97c7ec83a3ba
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-24 01:45:02 +0200

    init: fenix rustNightly (June 2025)

--- pkgs/msedit-rs/default.nix
+++ pkgs/msedit-rs/default.nix
@@ -1,27 +1,37 @@
 {
+  pkgs,
   lib, 
-  rustPlatform,
+  makeRustPlatform,
   icu,
   makeWrapper,
   fetchFromGitHub
 }:
 
+let
+  ### pin rust nightly with fenix project (fenix : https://github.com/nix-community/fenix)
+  ### (based on https://github.com/dtomvan/nur-packages/blob/0d9b84b67786425c259ecdb83f7a88165f06395d/pkgs/microsoft-edit/package.nix#L14)
+  fenixToolchain = import ../rustSupport/buildSupportNightly.nix { inherit pkgs;};
+  rustPlatform = makeRustPlatform {
+    inherit (fenixToolchain) cargo rustc;
+  };
+in
+
 rustPlatform.buildRustPackage rec {
   pname = "msedit";
-  version = "1.1.0";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "edit";
     rev = "v${version}";
-    hash = "sha256-ubdZynQVwCYhZA/z4P/v6aX5rRG9BCcWKih/PzuPSeE=";
+    hash = "sha256-G5U5ervW1NAQY/fnwOWv1FNuKcP+HYcAW5w87XHqgA8=";
   };
   
   ### Use rust nightly from this variable
   env.RUSTC_BOOTSTRAP = 1;
 
   ### hash from cargo.lock to the edit repository
-  cargoHash = "sha256-qT4u8LuKX/QbZojNDoK43cRnxXwMjvEwybeuZIK6DQQ=";
+  cargoHash = "sha256-ceAaaR+N03Dq2MHYel4sHDbbYUOr/ZrtwqJwhaUbC2o=";
   
   ### Disable install check (found here: https://github.com/RossSmyth/nixpkgs/blob/4f09843cb4edf26fac02a021eecfd9e5af0e5206/pkgs/by-name/ms/ms-edit/package.nix)
   doInstallCheck = false;
