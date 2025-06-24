5938be3dfee597889b90f2ff89c02476d5058d45
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-24 02:11:44 +0200

    move programs to related section

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,51 +2,51 @@
 
 {
  ### Utilities
- sshrm = callPackage ./sshrm {}; 
- GLFfetch = callPackage ./GLFfetch {};
- GLFfetch-glfos = callPackage ./GLFfetch { glfIcon = "GLFos"; };
+ sshrm = callPackage ./tools/sshrm {}; 
+ GLFfetch = callPackage ./misc/GLFfetch {};
+ GLFfetch-glfos = callPackage ./misc/GLFfetch { glfIcon = "GLFos"; };
 
  ### Editor set
  editor = {
-   msedit-bin = callPackage ./msedit-bin {};
-   msedit-rs = callPackage ./msedit-rs {};
+   msedit-bin = callPackage ./editor/msedit-bin {};
+   msedit-rs = callPackage ./editor/msedit-rs {};
  };
 
  ### dev set
  dev = {
    ### Without option (with clang variant)
-   fhsEnv-shell = callPackage ./fhsEnv-shell {};
-   fhsEnv-shell-clang = callPackage ./fhsEnv-shell { useClang = true; };
+   fhsEnv-shell = callPackage ./tools/fhsEnv-shell {};
+   fhsEnv-shell-clang = callPackage ./tools/fhsEnv-shell { useClang = true; };
 
    ### With kernel-tools
-   fhsEnv-shell-krnl = callPackage ./fhsEnv-shell { kernel-tools = true; };
+   fhsEnv-shell-krnl = callPackage ./tools/fhsEnv-shell { kernel-tools = true; };
    
    ### With buildroot-tools
-   fhsEnv-shell-buildroot = callPackage ./fhsEnv-shell { buildroot-tools = true; };
+   fhsEnv-shell-buildroot = callPackage ./tools/fhsEnv-shell { buildroot-tools = true; };
    
    ### With debian-tools
-   fhsEnv-shell-deb-tools = callPackage ./fhsEnv-shell { debian-tools = true; };
-   fhsEnv-shell-deb-tools-clang = callPackage ./fhsEnv-shell { debian-tools = true; useClang = true; };
-   fhsEnv-shell-deb-tools-krnl = callPackage ./fhsEnv-shell { debian-tools = true; kernel-tools = true; };
+   fhsEnv-shell-deb-tools = callPackage ./tools/fhsEnv-shell { debian-tools = true; };
+   fhsEnv-shell-deb-tools-clang = callPackage ./toolsfhsEnv-shell { debian-tools = true; useClang = true; };
+   fhsEnv-shell-deb-tools-krnl = callPackage ./tools/fhsEnv-shell { debian-tools = true; kernel-tools = true; };
    
    ### With redhat-tools
-   fhsEnv-shell-rh-tools = callPackage ./fhsEnv-shell { redhat-tools = true; };
-   fhsEnv-shell-rh-tools-clang = callPackage ./fhsEnv-shell { redhat-tools = true; useClang = true; };
-   fhsEnv-shell-rh-tools-krnl = callPackage ./fhsEnv-shell { redhat-tools = true; kernel-tools = true; };
+   fhsEnv-shell-rh-tools = callPackage ./tools/fhsEnv-shell { redhat-tools = true; };
+   fhsEnv-shell-rh-tools-clang = callPackage ./tools/fhsEnv-shell { redhat-tools = true; useClang = true; };
+   fhsEnv-shell-rh-tools-krnl = callPackage ./tools/fhsEnv-shell { redhat-tools = true; kernel-tools = true; };
    
    ### All configuration (With or without clang)
-   fhsEnv-shell-all = callPackage ./fhsEnv-shell { kernel-tools = true; buildroot-tools = true; };
-   fhsEnv-shell-all-specific = callPackage ./fhsEnv-shell { redhat-tools = true; debian-tools = true; kernel-tools = true; buildroot-tools = true; };
-   fhsEnv-shell-all-specific-nokrnl = callPackage ./fhsEnv-shell { redhat-tools = true; debian-tools = true; buildroot-tools = true; useClang = true;};
+   fhsEnv-shell-all = callPackage ./tools/fhsEnv-shell { kernel-tools = true; buildroot-tools = true; };
+   fhsEnv-shell-all-specific = callPackage ./tools/fhsEnv-shell { redhat-tools = true; debian-tools = true; kernel-tools = true; buildroot-tools = true; };
+   fhsEnv-shell-all-specific-nokrnl = callPackage ./tools/fhsEnv-shell { redhat-tools = true; debian-tools = true; buildroot-tools = true; useClang = true;};
  };
 
  ### Theme sets
  theme = {
-   marble-shell-filled = callPackage ./marble-shell-filled {};
+   marble-shell-filled = callPackage ./theme/marble-shell-filled {};
    
    ### marble-shell to marble-shell-filled with warning when this attribute is called
    #marble-shell = let
-   #   buildPackage = callPackage ./marble-shell-filled {};
+   #   buildPackage = callPackage ./theme/marble-shell-filled {};
    # in 
    #   builtins.warn
    #   "[2024/03/12] marble-shell has been renamed to marble-shell-filled, consider migrating to this new name before deleting this attribute in 1 month. (nur.repos.minegameYTB.theme.marble-shell -> nur.repos.minegameYTB.theme.marble-shell-filled)"
