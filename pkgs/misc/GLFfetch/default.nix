076291c70708aede77e32c1f6bfcc656372c2121
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-01-05 23:40:32 +0100

    Revert "GLFfetch 0-unstable-2025-09-01 -> 1.2.alpha"
    This reverts commit 2e29b5218d9855a0cb4d6f06d6bc56a2fd4c079d.

--- pkgs/misc/GLFfetch/default.nix
+++ pkgs/misc/GLFfetch/default.nix
@@ -1,80 +1,117 @@
-{ lib, stdenvNoCC, substituteAll, writeScriptBin, makeWrapper, fastfetch, coreutils, gawk, bash, glfIcon ? "GLF" }:
+{
+  lib,
+  stdenvNoCC,
+  substituteAll,
+  writeScriptBin,
+  fetchurl,
+  makeWrapper,
+  fastfetch,
+  coreutils,
+  gawk,
+  bash,
+  glfIcon ? "GLF", # Use GLF icon or GLFos icon (to change icon) (How to create an overlay with this expression ?)
+}:
 
-let
-  srcUrl = "https://framagit.org/gaming-linux-fr/glf-os/app-glf-os/glffetch/-/archive/main/glffetch-main.tar.gz";
-in
-
-assert lib.elem glfIcon [ "GLF" "GLFos" ] || throw "glfIcon must be either \"GLF\" or \"GLFos\" (got: ${glfIcon})";
+### sets the option to two choices (otherwise, throw error)
+assert
+  lib.elem glfIcon [
+    "GLF"
+    "GLFos"
+  ]
+  || throw "glfIcon must be either \"GLF\" or \"GLFos\" (got: ${glfIcon})";
 
 stdenvNoCC.mkDerivation rec {
   pname = "GLFfetch";
-  version = "1.2.alpha";
+  version = "0-unstable-2025-09-01"; # Last commit date
 
-  src = builtins.fetchTarball {
-    url = srcUrl;
-    sha256 = "1wg79rs1s4l15q3p5y6v8f7avgil59rdbag65g8npjvmv352s3gj";
+  src = fetchurl {
+    ### Use srcUrl as a url
+    url = "https://framagit.org/gaming-linux-fr/glf-os/app-glf-os/glffetch/-/archive/aa53e020d0da6a67662c62d564240dacf819e189/glffetch-aa53e020d0da6a67662c62d564240dacf819e189.tar.gz";
+    sha256 = "sha256-AgUQ4tmH4Wta+HuIvFZc89UHEdZcDiQdZiHsASCgDlE=";
   };
 
-  outputs = [ "out" "assets" ];
+  outputs = [
+    "out"
+    "assets"
+  ];
   outputsToInstall = outputs;
 
-  buildInputs = [ fastfetch bash coreutils gawk ];
   nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [
+    fastfetch
+    bash.out
+    coreutils
+    gawk
+  ];
 
   postPatch = ''
+    ### Patch path in upstream archive
+    ### *this command uses relative path in this context*
+
+    ### Patch challenge.jsonc
     substituteInPlace challenge.jsonc \
       --replace-warn "~/.config/fastfetch/GLFfetch/GLF.png" "$assets/share/${pname}/${glfIcon}.png" \
       --replace-warn "~/.config/fastfetch/GLFfetch" "$assets/share/${pname}" \
       --replace-warn "󰣇" "" \
-      --replace-warn "/bin/bash" "${bash}/bin/bash"
+      --replace-warn "/bin/bash" "${bash.out}/bin/bash"
 
     substituteInPlace scripts/challenge.sh \
-      --replace-warn "/bin/bash" "${bash}/bin/bash" \
+      --replace-warn "/bin/bash" "${bash.out}/bin/bash" \
       --replace-warn "~/.config/fastfetch/GLFfetch" "$assets/share/${pname}"
 
     substituteInPlace scripts/completion.sh \
-      --replace-warn "/bin/bash" "${bash}/bin/bash" \
+      --replace-warn "/bin/bash" "${bash.out}/bin/bash" \
       --replace-warn "~/.config/fastfetch/GLFfetch" "$assets/share/${pname}"
 
     substituteInPlace scripts/icon.sh \
-      --replace-warn "/bin/bash" "${bash}/bin/bash" \
+      --replace-warn "/bin/bash" "${bash.out}/bin/bash" \
       --replace-warn '"$HOME"/.config/fastfetch/GLFfetch' "$assets/share/${pname}"
 
     substituteInPlace scripts/install_date.sh \
-      --replace-warn "/bin/bash" "${bash}/bin/bash" \
+      --replace-warn "/bin/bash" "${bash.out}/bin/bash" \
       --replace-warn "~/.config/fastfetch/GLFfetch" "$assets/share/${pname}"
 
-    sed -i '1a PATH="${coreutils}/bin:${gawk}/bin:"' scripts/vars.sh
+    ### Add path to vars.sh
+    sed -i '1a PATH="${coreutils}/bin:${gawk}/bin"' scripts/vars.sh
+
     substituteInPlace scripts/vars.sh \
-      --replace-warn "/bin/bash" "${bash}/bin/bash"
+      --replace-warn "/bin/bash" "${bash.out}/bin/bash"
   '';
 
   installPhase = ''
     mkdir -p $out/bin $assets/share/${pname}
     mkdir -p $out/share/doc/${pname}
 
+    ### Copy all files
     cp -r . $assets/share/${pname}/
 
-    mv $assets/share/${pname}/LICENSE $out/share/doc/${pname}/ 2>/dev/null || true
-    mv $assets/share/${pname}/README.md $out/share/doc/${pname}/ 2>/dev/null || true
+    ### Move some files to doc
+    mv $assets/share/${pname}/LICENSE $out/share/doc/${pname}/
+    mv $assets/share/${pname}/README.md $out/share/doc/${pname}/
 
+    ### Symlink "assets" output to "out" (to make them accessible to profile that GLFfetch is installed)
     ln -s $assets/share/${pname} $out/share/${pname}
 
     ${lib.optionalString (glfIcon == "GLFos") ''
-      if [ -f ${./logo.png} ]; then
-        ln -sf ${./logo.png} $assets/share/${pname}/${glfIcon}.png
-        rm -f $assets/share/${pname}/GLF.png
-      fi
+      ### Link logo from nix store
+      ln -s ${./logo.png} $assets/share/${pname}/${glfIcon}.png
+      rm $assets/share/${pname}/GLF.png
     ''}
 
     chmod +x $assets/share/${pname}/scripts/*.sh
+
     makeWrapper ${fastfetch}/bin/fastfetch $out/bin/GLFfetch \
       --add-flags "--config $assets/share/${pname}/challenge.jsonc" \
-      --prefix PATH : ${lib.makeBinPath [ coreutils gawk ]}
+      --set PATH ${
+        lib.makeBinPath [
+          coreutils
+          gawk
+        ]
+      }
   '';
 
   meta = {
-    description = "A customized neofetch config file built for the GLF Linux challenges";
+    description = "A customized neofetch config file built for the GLF Linux challenges (github.com/minegameYTB/GLFfetch-nixos is it's fork)";
     homepage = "https://framagit.org/gaming-linux-fr/glf-os";
     license = lib.licenses.mit;
     mainProgram = "GLFfetch";
