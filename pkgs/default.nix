d32d2bbd0cd9953360d92582401718f2f59ef085
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-08-31 14:42:54 +0200

    fhsEnv-shell: i686 mirror via override (DRY)
    - i686.fhsEnv-shell = dev.fhsEnv-shell.override { i686Support = true; }
    - inner variants override the i686 base, no longer callPackage with duplicated flags

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -66,68 +66,45 @@ rec {
 
     ### 32-bit mirror (i686) - strict mirror of dev.* with i686Support
     i686 = rec {
-      fhsEnv-shell = callPackage ./tools/fhsEnv-shell { i686Support = true; };
-      fhsEnv-shell-clang = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
-        useClang = true;
-      };
-      fhsEnv-shell-krnl = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
-        kernel-tools = true;
-      };
-      fhsEnv-shell-buildroot = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
-        buildroot-tools = true;
-      };
-      fhsEnv-shell-deb-tools = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
-        debian-tools = true;
-      };
-      fhsEnv-shell-deb-tools-clang = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
+      fhsEnv-shell = dev.fhsEnv-shell.override { i686Support = true; };
+      fhsEnv-shell-clang = fhsEnv-shell.override { useClang = true; };
+      fhsEnv-shell-krnl = fhsEnv-shell.override { kernel-tools = true; };
+      fhsEnv-shell-buildroot = fhsEnv-shell.override { buildroot-tools = true; };
+      fhsEnv-shell-deb-tools = fhsEnv-shell.override { debian-tools = true; };
+      fhsEnv-shell-deb-tools-clang = fhsEnv-shell.override {
         debian-tools = true;
         useClang = true;
       };
-      fhsEnv-shell-deb-tools-krnl = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
+      fhsEnv-shell-deb-tools-krnl = fhsEnv-shell.override {
         debian-tools = true;
         kernel-tools = true;
       };
-      fhsEnv-shell-rh-tools = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
-        redhat-tools = true;
-      };
-      fhsEnv-shell-rh-tools-clang = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
+      fhsEnv-shell-rh-tools = fhsEnv-shell.override { redhat-tools = true; };
+      fhsEnv-shell-rh-tools-clang = fhsEnv-shell.override {
         redhat-tools = true;
         useClang = true;
       };
-      fhsEnv-shell-rh-tools-krnl = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
+      fhsEnv-shell-rh-tools-krnl = fhsEnv-shell.override {
         redhat-tools = true;
         kernel-tools = true;
       };
-      fhsEnv-shell-all = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
+      fhsEnv-shell-all = fhsEnv-shell.override {
         kernel-tools = true;
         buildroot-tools = true;
       };
-      fhsEnv-shell-all-specific = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
+      fhsEnv-shell-all-specific = fhsEnv-shell.override {
         kernel-tools = true;
         buildroot-tools = true;
         debian-tools = true;
         redhat-tools = true;
       };
-      fhsEnv-shell-all-specific-nokrnl = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
+      fhsEnv-shell-all-specific-nokrnl = fhsEnv-shell.override {
         buildroot-tools = true;
         debian-tools = true;
         redhat-tools = true;
         useClang = true;
       };
-      fhsEnv-shell-all-clang = callPackage ./tools/fhsEnv-shell {
-        i686Support = true;
+      fhsEnv-shell-all-clang = fhsEnv-shell.override {
         useClang = true;
         kernel-tools = true;
         buildroot-tools = true;
